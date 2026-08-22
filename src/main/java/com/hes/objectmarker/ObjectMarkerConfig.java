35846d1ba47b03a83567e3e9e91b36d16c08d22f
tommyvercettiseh <tommyvercettiseh@gmail.com>
2026-08-22 19:31:25 +0200

    Add simple label box settings

--- src/main/java/com/hes/objectmarker/ObjectMarkerConfig.java
+++ src/main/java/com/hes/objectmarker/ObjectMarkerConfig.java
@@ -4,11 +4,33 @@ import java.awt.Color;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.Range;
 
 @ConfigGroup(ObjectMarkerConfig.GROUP)
 public interface ObjectMarkerConfig extends Config
 {
 	String GROUP = "objectmarker";
+
+	@ConfigItem(
+		keyName = "labelBoxFill",
+		name = "Label box fill",
+		description = "Show a cyan box behind tagged object labels"
+	)
+	default boolean labelBoxFill()
+	{
+		return true;
+	}
+
+	@Range(min = 0, max = 100)
+	@ConfigItem(
+		keyName = "labelBoxOpacity",
+		name = "Label box opacity",
+		description = "Opacity of the cyan label box"
+	)
+	default int labelBoxOpacity()
+	{
+		return 85;
+	}
 
 	@ConfigItem(
 		keyName = "markOtherPlayers",
