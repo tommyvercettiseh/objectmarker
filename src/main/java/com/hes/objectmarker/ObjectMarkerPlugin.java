b6e1a907f598c645a1df2583356b8b0c5f842a68
tommyvercettiseh <tommyvercettiseh@gmail.com>
2026-08-22 13:10:13 +0200

    Wire player marker config and overlay

--- src/main/java/com/hes/objectmarker/ObjectMarkerPlugin.java
+++ src/main/java/com/hes/objectmarker/ObjectMarkerPlugin.java
@@ -1,14 +1,16 @@
 package com.hes.objectmarker;
 
+import com.google.inject.Provides;
 import javax.inject.Inject;
+import net.runelite.client.config.ConfigManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
 
 @PluginDescriptor(
 	name = "Object Marker",
-	description = "Marks selected game objects with a simple text label",
-	tags = {"object", "marker", "fire"}
+	description = "Marks selected game objects and optionally highlights other players",
+	tags = {"object", "marker", "highlight", "players"}
 )
 public class ObjectMarkerPlugin extends Plugin
 {
@@ -16,17 +18,28 @@ public class ObjectMarkerPlugin extends Plugin
 	private OverlayManager overlayManager;
 
 	@Inject
-	private ObjectMarkerOverlay overlay;
+	private ObjectMarkerOverlay objectMarkerOverlay;
+
+	@Inject
+	private PlayerMarkerOverlay playerMarkerOverlay;
+
+	@Provides
+	ObjectMarkerConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(ObjectMarkerConfig.class);
+	}
 
 	@Override
 	protected void startUp()
 	{
-		overlayManager.add(overlay);
+		overlayManager.add(objectMarkerOverlay);
+		overlayManager.add(playerMarkerOverlay);
 	}
 
 	@Override
 	protected void shutDown()
 	{
-		overlayManager.remove(overlay);
+		overlayManager.remove(objectMarkerOverlay);
+		overlayManager.remove(playerMarkerOverlay);
 	}
 }
