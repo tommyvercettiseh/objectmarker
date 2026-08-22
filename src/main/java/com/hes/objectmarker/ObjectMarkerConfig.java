fc1c20208b3d3fe197aad526020447bd4f73a014
tommyvercettiseh <tommyvercettiseh@gmail.com>
2026-08-22 13:10:00 +0200

    Add player marker configuration

---
+++ src/main/java/com/hes/objectmarker/ObjectMarkerConfig.java
@@ -0,0 +1,32 @@
+package com.hes.objectmarker;
+
+import java.awt.Color;
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+@ConfigGroup(ObjectMarkerConfig.GROUP)
+public interface ObjectMarkerConfig extends Config
+{
+	String GROUP = "objectmarker";
+
+	@ConfigItem(
+		keyName = "markOtherPlayers",
+		name = "Mark other players",
+		description = "Highlight all other visible players"
+	)
+	default boolean markOtherPlayers()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		keyName = "otherPlayerColor",
+		name = "Player colour",
+		description = "Colour used to highlight other players"
+	)
+	default Color otherPlayerColor()
+	{
+		return new Color(57, 255, 20);
+	}
+}
