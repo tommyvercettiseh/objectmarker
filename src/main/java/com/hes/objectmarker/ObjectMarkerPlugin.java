9cf9072232c6e69e0a57c8ac1c5a4528ed8eb0ae
tommyvercettiseh <tommyvercettiseh@gmail.com>
2026-08-22 13:16:01 +0200

    Add Object Marker sidebar navigation

--- src/main/java/com/hes/objectmarker/ObjectMarkerPlugin.java
+++ src/main/java/com/hes/objectmarker/ObjectMarkerPlugin.java
@@ -1,16 +1,21 @@
 package com.hes.objectmarker;
 
 import com.google.inject.Provides;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import javax.inject.Inject;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.ClientToolbar;
+import net.runelite.client.ui.NavigationButton;
 import net.runelite.client.ui.overlay.OverlayManager;
 
 @PluginDescriptor(
 	name = "Object Marker",
-	description = "Marks selected game objects and optionally highlights other players",
-	tags = {"object", "marker", "highlight", "players"}
+	description = "Live markers for objects, ground items, NPCs and players",
+	tags = {"object", "marker", "highlight", "players", "npc", "ground"}
 )
 public class ObjectMarkerPlugin extends Plugin
 {
@@ -18,10 +23,19 @@ public class ObjectMarkerPlugin extends Plugin
 	private OverlayManager overlayManager;
 
 	@Inject
+	private ClientToolbar clientToolbar;
+
+	@Inject
 	private ObjectMarkerOverlay objectMarkerOverlay;
 
 	@Inject
 	private PlayerMarkerOverlay playerMarkerOverlay;
+
+	@Inject
+	private MarkerStore markerStore;
+
+	private ObjectMarkerPanel panel;
+	private NavigationButton navigationButton;
 
 	@Provides
 	ObjectMarkerConfig provideConfig(ConfigManager configManager)
@@ -34,6 +48,15 @@ public class ObjectMarkerPlugin extends Plugin
 	{
 		overlayManager.add(objectMarkerOverlay);
 		overlayManager.add(playerMarkerOverlay);
+
+		panel = new ObjectMarkerPanel(markerStore);
+		navigationButton = NavigationButton.builder()
+			.tooltip("Object Marker")
+			.icon(createIcon())
+			.priority(5)
+			.panel(panel)
+			.build();
+		clientToolbar.addNavigation(navigationButton);
 	}
 
 	@Override
@@ -41,5 +64,23 @@ public class ObjectMarkerPlugin extends Plugin
 	{
 		overlayManager.remove(objectMarkerOverlay);
 		overlayManager.remove(playerMarkerOverlay);
+		if (navigationButton != null)
+		{
+			clientToolbar.removeNavigation(navigationButton);
+		}
+		panel = null;
+		navigationButton = null;
+	}
+
+	private BufferedImage createIcon()
+	{
+		BufferedImage image = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D graphics = image.createGraphics();
+		graphics.setColor(new Color(255, 140, 0));
+		graphics.fillOval(2, 2, 12, 12);
+		graphics.setColor(Color.WHITE);
+		graphics.drawOval(4, 4, 8, 8);
+		graphics.dispose();
+		return image;
 	}
 }
